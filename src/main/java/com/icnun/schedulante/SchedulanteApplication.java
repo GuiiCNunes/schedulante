f7c331025edeb1c1fd632becb23161cca50df735
guiicnunes <guiicnunes@gmail.com>
2024-07-15 21:20:33 -0300

    feat: aplicacao principal

---
+++ src/main/java/com/icnun/schedulante/SchedulanteApplication.java
@@ -0,0 +1,13 @@
+package com.icnun.schedulante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulanteApplication.class, args);
+	}
+
+}
