f225b97300d00d22415f8494d48661f2f4fc5b02
guiicnunes <guiicnunes@gmail.com>
2024-07-18 19:34:44 -0300

    feat: teste conteudo

---
+++ src/test/java/com/icnun/schedulante/model/TestConteudo.java
@@ -0,0 +1,15 @@
+package com.icnun.schedulante.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+public class TestConteudo {
+    @Test
+    @DisplayName("Testa a inicialização da classe")
+    public void testaInicializacaoDaClasse() {
+        Object conteudo = new Conteudo();
+        assertInstanceOf(Conteudo.class, conteudo);
+    }
+}
