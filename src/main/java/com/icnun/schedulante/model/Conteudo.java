3a24ffe61cd33c8acca61b14841656e5b86128f3
Guilherme Nunes <guiicnunes@gmail.com>
2024-07-18 20:02:41 -0300

    feat: novo construtor

--- src/main/java/com/icnun/schedulante/model/Conteudo.java
+++ src/main/java/com/icnun/schedulante/model/Conteudo.java
@@ -4,6 +4,10 @@ public class Conteudo {
     private String link;
     public Conteudo(){}
 
+    public Conteudo(String link) {
+        this.link = link;
+    }
+
     public String getLink() {
         return link;
     }
