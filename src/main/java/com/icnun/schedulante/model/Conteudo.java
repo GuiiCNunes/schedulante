785fca5d409c56c54015942f06de5e8550b274d7
Guilherme Nunes <guiicnunes@gmail.com>
2024-07-18 19:53:59 -0300

    feat: atributo link do Conteudo

--- src/main/java/com/icnun/schedulante/model/Conteudo.java
+++ src/main/java/com/icnun/schedulante/model/Conteudo.java
@@ -1,5 +1,14 @@
 package com.icnun.schedulante.model;
 
 public class Conteudo {
+    private String link;
     public Conteudo(){}
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
 }
