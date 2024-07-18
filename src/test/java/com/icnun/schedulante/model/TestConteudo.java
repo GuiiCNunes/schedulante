30cb498fa614051cfda66a9a80d04126ff4a954b
Guilherme Nunes <guiicnunes@gmail.com>
2024-07-18 19:47:25 -0300

    feat: agrupamento dos testes de conteudo com o uso de Nested

--- src/test/java/com/icnun/schedulante/model/TestConteudo.java
+++ src/test/java/com/icnun/schedulante/model/TestConteudo.java
@@ -1,15 +1,20 @@
 package com.icnun.schedulante.model;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 public class TestConteudo {
-    @Test
-    @DisplayName("Testa a inicialização da classe")
-    public void testaInicializacaoDaClasse() {
-        Object conteudo = new Conteudo();
-        assertInstanceOf(Conteudo.class, conteudo);
+    @Nested
+    @DisplayName("Testa a inicializacao")
+    class TestInicializacao {
+        @Test
+        @DisplayName("Testa a inicialização da classe sem parâmetros")
+        public void testaInicializacaoDaClasse() {
+            Object conteudo = new Conteudo();
+            assertInstanceOf(Conteudo.class, conteudo);
+        }
     }
 }
