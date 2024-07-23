a345be1a065094d18ef160b159bed1393d58a868
Guilherme Nunes <guiicnunes@gmail.com>
2024-07-22 21:32:34 -0300

    feat: ConteudoDao

---
+++ src/main/java/com/icnun/schedulante/dao/ConteudoDao.java
@@ -0,0 +1,7 @@
+package com.icnun.schedulante.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConteudoDao extends DaoInterface{
+}
---
+++ src/main/java/com/icnun/schedulante/dao/impl/ConteudoDaoImpl.java
@@ -0,0 +1,9 @@
+package com.icnun.schedulante.dao.impl;
+
+import com.icnun.schedulante.dao.ConteudoDao;
+import com.icnun.schedulante.dao.DaoAbstrato;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ConteudoDaoImpl extends DaoAbstrato implements ConteudoDao  {
+}
